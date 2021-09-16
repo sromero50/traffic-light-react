3edc994b068dfc41da9ca0d98f3a0d8b4166eb1f
sromero50 <seba98asd@hotmail.com>
2021-09-16 00:42:58 +0000

    first upload

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+
+	const interval = setInterval(function() {
+		if (color === "red") {
+			setColor("yellow");
+		} else if (color === "yellow") {
+			setColor("green");
+		} else if (color === "green") {
+			setColor("red");
+		}
+	}, 3000);
+
+	useEffect(() => {
+		return () => {
+			clearInterval(interval);
+		};
+	});
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<div className="line"></div>
+			<div className="traffic">
+				<div
+					className={"red light" + (color === "red" ? " redOn" : "")}
+					onClick={() => setColor("red")}></div>
+				<div
+					className={
+						"yellow light" + (color === "yellow" ? " yellowOn" : "")
+					}
+					onClick={() => setColor("yellow")}></div>
+				<div
+					className={
+						"green light" + (color === "green" ? " greenOn" : "")
+					}
+					onClick={() => setColor("green")}></div>
+			</div>
 		</div>
 	);
+
+	// const lightsOn = () => {
+	// 	document.querySelector(".red").addEventListener("click", e => {
+	// 		setColor("red");
+	// 		console.log(color);
+	// 	});
+	// 	document.querySelector(".yellow").addEventListener("click", e => {
+	// 		setColor("yellow");
+	// 		console.log(color);
+	// 	});
+	// 	document.querySelector(".green").addEventListener("click", e => {
+	// 		setColor("green");
+	// 		console.log(color);
+	// 	});
+
+	// 	if (color === "red") {
+	// 		document.querySelector(".red").classList.add("redOn");
+	// 		document.querySelector(".yellow").classList.remove("yellowOn");
+	// 		document.querySelector(".green").classList.remove("greenOn");
+	// 	} else if (color === "yellow") {
+	// 		document.querySelector(".yellow").classList.add("yellowOn");
+	// 		document.querySelector(".red").classList.remove("redOn");
+	// 		document.querySelector(".green").classList.remove("greenOn");
+	// 	} else if (color === "green") {
+	// 		document.querySelector(".green").classList.add("greenOn");
+	// 		document.querySelector(".red").classList.remove("redOn");
+	// 		document.querySelector(".yellow").classList.remove("yellowOn");
+	// 	}
+	// };
+
+	// const interval = setInterval(function() {
+	// 	if (color === "red") {
+	// 		setColor("yellow");
+	// 		document.querySelector(".yellow").classList.add("yellowOn");
+	// 		document.querySelector(".red").classList.remove("redOn");
+	// 		document.querySelector(".green").classList.remove("greenOn");
+	// 	} else if (color === "yellow") {
+	// 		setColor("green");
+	// 		document.querySelector(".green").classList.add("greenOn");
+	// 		document.querySelector(".red").classList.remove("redOn");
+	// 		document.querySelector(".yellow").classList.remove("yellowOn");
+	// 	} else if (color === "green") {
+	// 		setColor("red");
+	// 		document.querySelector(".red").classList.add("redOn");
+	// 		document.querySelector(".yellow").classList.remove("yellowOn");
+	// 		document.querySelector(".green").classList.remove("greenOn");
+	// 	}
+	// }, 3000);
+
+	// return (
+	// 	<div className="container-fluid">
+	// 		<div className="line"></div>
+	// 		<div className="traffic">
+	// 			<div className="red light" onClick={lightsOn}></div>
+	// 			<div className="yellow light" onClick={lightsOn}></div>
+	// 			<div className="green light" onClick={lightsOn}></div>
+	// 		</div>
+	// 	</div>
+	// );
 };
 
 export default Home;
